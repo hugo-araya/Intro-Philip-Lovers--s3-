54069d662b8dc7386ad885b8468d1bf14738a22b
Hugo Araya C <42989121+hugo-araya@users.noreply.github.com>
2023-04-19 12:06:36 -0400

    19-04

---
+++ banco.py
@@ -0,0 +1,6 @@
+# Entero cap_invertir
+# Real ganancia
+cap_invertir = input('Ingrese el capital: ')
+cap_invertir = int(cap_invertir)
+ganancia = cap_invertir * 0.02 
+print('La ganancia obtenida es',ganancia)
